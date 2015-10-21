ad6533474fd2c19e7caf3b52738b01b446fb9736
lcharlie7 <lcharlie93@gmail.com>
2015-10-21 02:04:10 +0200

    "first commit"

---
+++ MyMediaPlayer/MainWindow.xaml.cs
@@ -0,0 +1,70 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.IO;
+
+
+namespace MyMediaPlayer
+{
+    /// <summary>
+    /// Logique d'interaction pour MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private string mediaName;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void button4_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog ofd = new OpenFileDialog();
+            ofd.AddExtension = true;
+            ofd.DefaultExt = "*.*";
+            ofd.Filter = "Media(*.*)|*.*";
+            ofd.ShowDialog();
+            if (ofd.FileName != "")
+            {
+                mediaElement1.MediaOpened += new RoutedEventHandler(mediaElement1_MediaOpened);
+                mediaElement1.Source = new Uri(ofd.FileName);
+                mediaName = mediaElement1.Source.ToString();
+            }
+        }
+
+        private void button1_Click(object sender, RoutedEventArgs e)
+        {
+            mediaElement1.Play();
+        }
+
+        private void button3_Click(object sender, RoutedEventArgs e)
+        {
+            mediaElement1.Stop();
+        }
+
+        private void button2_Click(object sender, RoutedEventArgs e)
+        {
+            mediaElement1.Pause();
+
+        }
+
+        void mediaElement1_MediaOpened(object sender, RoutedEventArgs e)
+        {
+
+            label1.Content = System.IO.Path.GetFileName(mediaElement1.Source.LocalPath);
+        }
+    }
+}
